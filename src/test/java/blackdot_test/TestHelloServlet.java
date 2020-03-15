129c55f8697d2f662e61851d51fc23732e0b3c58
Simon Clarkstone <simon_clarkstone@yahoo.co.uk>
2020-03-15 03:15:47 +0000

    Got basic Java webapp to build.

---
+++ src/test/java/blackdot_test/TestHelloServlet.java
@@ -0,0 +1,12 @@
+package blackdot_test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestHelloServlet {
+    @Test
+    public void testNothing() {
+        assertTrue(true);
+    }
+}
